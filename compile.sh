ffe72bb3f7fe064f00024abb09ed1fac54953fd0
Jan Sevrin <jan@sevrin.com>
2023-03-24 15:56:05 +0100

    Made compile.sh and created folder for Linux-specific configurations for this project

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Compiling into a binary using pyinstaller..."
+#python3 -m PyInstaller app/app.py --name can-data-logger --onefile --hiden-import can.interfaces.socketcan
+
+
+echo "Compilation succesful, cleaning up..."
